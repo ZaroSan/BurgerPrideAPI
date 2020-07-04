362e7a71761de891f6a63f238432172cfd7e296a
ZaroSan <martin2609393@gmail.com>
2020-07-04 02:56:14 +0200

    fix pour ingredientList car c'est une table de liaison

--- controller/IngredientListController.php
+++ controller/IngredientListController.php
@@ -3,5 +3,58 @@
 
 class IngredientListController extends Controller
 {
+    private $ingredient;
+    private $sandwich;
 
+    function __construct($request)
+    {
+        $this->data=$request->data;
+        $this->method=$request->method;
+
+        $this->controller=ucfirst($request->controller);
+        $this->id=null;
+        for($i=0;$i<sizeof($request->params);$i++){
+            if($request->params[$i]=='ingredient'){
+                $this->ingredient=$request->params[$i+1];
+            }
+            elseif ($request->params[$i]=='sandwich'){
+                $this->sandwich=$request->params[$i+1];
+            }
+        }
+    }
+    public function delete()
+    {
+        $this->loadModel('IngredientList');
+        if(isset($this->sandwich) && isset($this->ingredient)){
+            $this->IngredientList->deleted($this->ingredient,$this->sandwich);
+        }
+        elseif (!isset($this->sandwich) && isset($this->ingredient)){
+            $this->IngredientList->deleteIngredient($this->ingredient);
+        }
+        elseif (isset($this->sandwich) && !isset($this->ingredient)){
+            $this->IngredientList->deleteSandwich($this->sandwich);
+        }
+    }
+    public function put()
+    {
+
+    }
+    public function get()
+    {
+        $d['IngredientLists']=null;
+        $this->loadModel('IngredientList');
+        if(isset($this->sandwich) && isset($this->ingredient)){
+            $d['IngredientLists']=$this->IngredientList->read($this->ingredient,$this->sandwich);
+        }
+        elseif (!isset($this->sandwich) && isset($this->ingredient)){
+            $d['IngredientLists']=$this->IngredientList->readIngredient($this->ingredient);
+        }
+        elseif (isset($this->sandwich) && !isset($this->ingredient)){
+            $d['IngredientLists']=$this->IngredientList->readSandwich($this->sandwich);
+        }
+        else{
+            $d['IngredientLists']= $this->IngredientList->readAll();
+        }
+        print_r(json_encode($d));
+    }
 }
--- model/IngredientList.php
+++ model/IngredientList.php
@@ -4,4 +4,40 @@
 class IngredientList extends Model
 {
     public $table ='ingredients_lists';
+    public $idIngredient='id_ingredient';
+    public $idSandwich='id_sandwich';
+
+    public function deleted($idIngredient,$idSandwich){
+        $sql="DELETE FROM {$this->table} WHERE {$this->idSandwich} = {$idSandwich} AND {$this->idIngredient} = {$idIngredient}";
+        $this->db->query($sql);
+    }
+    public function deleteIngredient($id){
+        $sql="DELETE FROM {$this->table} WHERE {$this->idIngredient} = {$id}";
+        $this->db->query($sql);
+    }
+    public function deleteSandwich($id){
+        $sql="DELETE FROM {$this->table} WHERE {$this->idSandwich} = {$id} ";
+        $this->db->query($sql);
+    }
+    public function readIngredient($id){
+        $sql="SELECT * FROM {$this->table} WHERE {$this->idIngredient} = {$id} ";
+        $pre=$this->db->prepare($sql);
+        $pre->execute();
+
+        return $pre->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function readSandwich($id){
+        $sql="SELECT * FROM {$this->table} WHERE {$this->idSandwich} = {$id} ";
+        $pre=$this->db->prepare($sql);
+        $pre->execute();
+
+        return $pre->fetchAll(PDO::FETCH_OBJ);
+    }
+    public function read($idIngredient,$idSandwich){
+        $sql="SELECT * FROM {$this->table} WHERE {$this->idSandwich} = {$idSandwich} AND {$this->idIngredient} = {$idIngredient}";
+        $pre=$this->db->prepare($sql);
+        $pre->execute();
+
+        return $pre->fetchAll(PDO::FETCH_OBJ);
+    }
 }
