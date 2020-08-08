a146d53dd21b3acb582c8f418f99ae3f16291fe7
Mars073 <imars073@live.be>
2020-08-08 23:27:46 +0200

    fix empty route issue

--- core/Router.php
+++ core/Router.php
@@ -1,50 +1,51 @@
 <?php
+    class Router
+    {
+        static $routes = array();
+        static $prefixes = array();
 
-
-class Router
-{
-    static $routes=array();
-    static $prefixes=array();
-
-    static function prefix($url,$prefix){
-        self::$prefixes[$url]=$prefix;
-    }
-    static function parse($url,$request){
-
-        $url=trim($url,'/');
-        if(empty($url)){
-            $url=(Router::$routes[0]['url']);
+        static function prefix($url, $prefix)
+        {
+            self::$prefixes[$url] = $prefix;
         }
-        else{
-            foreach (Router::$routes as $value) {
-                # code...
 
-                if(preg_match($value['catcher'], $url,$match)){
-                    $request->controller= $value['controller'];
-                    $request->params=array();
-                    foreach ($value['params'] as $key=>$w) {
-                        # code...
-                        $request->params[$key]=$match[$key];
+        static function parse($url, $request)
+        {
+            $url = trim($url,'/');
+            if(empty($url))
+            {
+                if (count(Router::$routes) > 0)
+                {
+                    $url = Router::$routes[0]['url'];
+                }
+            }
+            else
+            {
+                foreach (Router::$routes as $value)
+                {
+                    if (preg_match($value['catcher'], $url, $match)){
+                        $request->controller= $value['controller'];
+                        $request->params=array();
+                        foreach ($value['params'] as $key=>$w) {
+                            # code...
+                            $request->params[$key]=$match[$key];
+                        }
+                        if(!empty($match['args'])){
+                            $request->params += explode('/', trim($match['args']),'/');
+                        }
+                        return $request;
                     }
-                    if(!empty($match['args'])){
-                        $request->params += explode('/', trim($match['args']),'/');
-                    }
-                    return $request;
                 }
             }
 
+            $params = explode('/', $url);
+            if(in_array($params[0], array_keys(self::$prefixes))){
+                $request->prefix = self::$prefixes[$params[0]];
+                array_shift($params);
+            }
+            $request->controller = $params[0];
+            $request->params = array_slice($params, 1);
+
+            return true;
         }
-
-        $params=explode('/', $url);
-        if(in_array($params[0], array_keys(self::$prefixes))){
-            $request->prefix=self::$prefixes[$params[0]];
-            array_shift($params);
-        }
-        $request->controller= $params[0];
-
-        $request->params= array_slice($params, 1);
-
-        return true;
-
     }
-}
