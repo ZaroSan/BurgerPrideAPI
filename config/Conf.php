ff60054116576c47957b1db06d0b8ad2a94019f1
Mars073 <imars073@live.be>
2020-08-08 17:41:27 +0200

    Add rechaptcha function

--- config/Conf.php
+++ config/Conf.php
@@ -1,18 +1,21 @@
 <?php
+    class Conf {
+        static $debug = 1;
 
-class Conf{
+        static $databases = array(
+            'local' => array(
+            'host' => 'localhost',
+            'database' => 'burgerpride',
+            'login' => 'root',
+            'password' => 'password'
+            )
+        );
+        static $confDB = 'local';
 
-    static $debug =1;
-    static $databases =array(
-        'local'=>array(
-            'host'=>'localhost',
-            'database'=>'burgerpride',
-            'login'=>'root',
-            'password'=>'password'
-        )
-    );
-    static $confDB='local';
-}
-
-
-
+        static $recaptcha = array(
+            'mobile' => array(
+                'secret' => 'secret-api-key'
+            )
+        );
+        static $defaultRecaptcha = 'mobile';
+    }
--- core/functions.php
+++ core/functions.php
@@ -1,21 +1,45 @@
 <?php
-	function debug($var){
-		if(Conf::$debug >0){
-			$debug = debug_backtrace();
+    function debug($var){
+        if(Conf::$debug >0){
+            $debug = debug_backtrace();
 
-			echo '<p><a href="#"><strong>'.$debug[0]['file'].'</strong> l.'.$debug[0]['line'].'</a></p>';
-			echo '<ol>';
-			foreach ($debug as $key => $value) {
-				if($key >0){
-					echo "<li><strong>".$value['file']."</strong> l.".$value['line']."</li>";
-				}
-			}
-			echo "</ol>";
-			echo "<pre>";
-			print_r($var);
-			echo "</pre>";
-		}
-		
-	}
+            echo '<p><a href="#"><strong>'.$debug[0]['file'].'</strong> l.'.$debug[0]['line'].'</a></p>';
+            echo '<ol>';
+            foreach ($debug as $key => $value) {
+                if($key >0){
+                    echo "<li><strong>".$value['file']."</strong> l.".$value['line']."</li>";
+                }
+            }
+            echo "</ol>";
+            echo "<pre>";
+            print_r($var);
+            echo "</pre>";
+        }
+        
+    }
 
-?>
+    function check_recaptcha($response, $secret = null) {
+        if ($secret == null) {
+            $secret = Conf::$recaptcha[Conf::$defaultRecaptcha];
+        }
+
+        $parameters = array(
+            'http' => array(
+                'header'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query(array(
+                    'secret'   => $secret,
+                    'response' => $response,
+                    'remoteip' => $_SERVER['REMOTE_ADDR']
+                ))
+            )
+        );
+        $context  = stream_context_create($parameters);
+        $response = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
+        if ($response == false) {
+            return false;
+        }
+        $r = json_decode($response, true);
+
+        return $r['success'];
+    }
